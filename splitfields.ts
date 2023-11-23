42390cb847fa5322673a671b1f1ecbbd6b821bfa
jeffmaddocks <jeff.maddocks@gmail.com>
2023-11-23 13:50:36 -0600

    added age categorization

--- splitfields.ts
+++ splitfields.ts
@@ -1,51 +1,85 @@
 async function main(workbook: ExcelScript.Workbook) {
     let sheet = workbook.getActiveWorksheet();
     let range = sheet.getUsedRange();
-
+  
     // Get the values in the range
     let rangeValues = range.getValues();
     let lst: string[] = [];
-
+  
     // Process the data
     for (let i = 0; i < rangeValues.length; i++) {  //looping through the rows
-        let thisrow: string[] = [];
-        for (let j = 0; j < rangeValues[i].length; j++) {  //looping through the columns
-            let colName = rangeValues[0][j]; //this is the name of the column (row 0 of the column)
-            let thiscell: string = typeof rangeValues[i][j] === 'string' ? rangeValues[i][j].toLowerCase() : String(rangeValues[i][j]);
-            if (colName == "Received Vaccine") {
-              let received = "";
-              if (thiscell.includes("covid")) { received = received + "Second (or later) dose of a COVID-19 vaccine; " }
-              if (thiscell.includes("flu")) { received = received + "Flu/Influenza vaccine; " }
-              if (thiscell.includes("pneumococcal")) { received = received + "Pneumococcal; " }
-              if (thiscell.includes("shingles")) { received = received + "Shingles; " }
-              if (thiscell.includes("rsv")) { received = received + "Respiratory Syncytial Virus (RSV); " }
-              if (thiscell.includes("other")) { received = received + "Other; " }
-              if (thiscell.includes("none")) { received = received + "None; " }
-              received = received.slice(0, -2);  // Trim the last two characters
-            
-              thisrow.push(received);
-            } else if (colName == "Patient Date of Birth") {
-              thisrow.push(thiscell);
+      let thisrow: string[] = [];
+      for (let j = 0; j < rangeValues[i].length; j++) {  //looping through the columns
+        let thiscell: string = typeof rangeValues[i][j] === 'string' ? rangeValues[i][j].toLowerCase() : String(rangeValues[i][j]);
+        let colName = rangeValues[0][j]; //this is the name of the column (row 0 of the column)
+        if (colName == "Received Vaccine") {
+          let received = "";
+          if (thiscell.includes("covid")) { received = received + "Second (or later) dose of a COVID-19 vaccine; " }
+          if (thiscell.includes("flu")) { received = received + "Flu/Influenza vaccine; " }
+          if (thiscell.includes("pneumococcal")) { received = received + "Pneumococcal; " }
+          if (thiscell.includes("shingles")) { received = received + "Shingles; " }
+          if (thiscell.includes("rsv")) { received = received + "Respiratory Syncytial Virus (RSV); " }
+          if (thiscell.includes("other")) { received = received + "Other; " }
+          if (thiscell.includes("none")) { received = received + "None; " }
+          received = received.slice(0, -2);  // Trim the last two characters
+          if (i == 0) { received = "Vaccinations received" }
+          thisrow.push(received);
+        } else if (colName == "Patient Date of Birth") {
+  
+          let birthDate = new Date(thiscell);
+          let today = new Date();
+          let age = today.getFullYear() - birthDate.getFullYear();
+          let m = today.getMonth() - birthDate.getMonth();
+          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) { //account for dates where the birthday hasn't happened yet
+            age--;
+          }
+  
+          let ageCategory: string;
+          if (age < 18) {
+            ageCategory = 'Under age 18';
+          } else if (age >= 18 && age <= 49) {
+            ageCategory = 'Age 18 - 49';
+          } else if (age >= 50 && age <= 54) {
+            ageCategory = 'Age 50 - 54';
+          } else if (age >= 55 && age <= 59) {
+            ageCategory = 'Age 55 - 59';
+          } else if (age >= 60 && age <= 64) {
+            ageCategory = 'Age 60 - 64';
+          } else if (age >= 65 && age <= 74) {
+            ageCategory = 'Age 65 - 74';
+          } else if (age >= 75 && age <= 84) {
+            ageCategory = 'Age 75 - 84';
+          } else if (age > 84) {
+            ageCategory = 'Age 85+';
+          } else {
+            if (i == 0) {
+              ageCategory = 'Age range';
             } else {
-              thisrow.push(thiscell);
+              ageCategory = 'I prefer not to answer';
             }
+          }
+  
+          thisrow.push(ageCategory);
+        } else {
+          thisrow.push(thiscell);
         }
-        lst.push(thisrow);
+      }
+      lst.push(thisrow);
     }
-
+  
     // Create a new sheet
     let newSheet = workbook.addWorksheet(sheet.getName() + '_split');
     // newSheet.getRange().setNumberFormat('@');
-
+  
     // Write data to the new sheet
     for (let i = 0; i < lst.length; i++) { //looping through the rows
-        for (let j = 0; j < lst[i].length; j++) { //looping through the columns
-            // Convert column number (j) to letter (A, B, C, ...)
-            let columnLetter = String.fromCharCode(65 + j); // 65 is the ASCII value for 'A'
-            if (j > 0) { //ensure that everything after column A is formatted as text
-                newSheet.getRange(`${columnLetter}${i + 1}`).setNumberFormat('@');
-            }
-            newSheet.getRange(`${columnLetter}${i + 1}`).setValue(lst[i][j]);
+      for (let j = 0; j < lst[i].length; j++) { //looping through the columns
+        // Convert column number (j) to letter (A, B, C, ...)
+        let columnLetter = String.fromCharCode(65 + j); // 65 is the ASCII value for 'A'
+        if (j > 0) { //ensure that everything after column A is formatted as text
+          newSheet.getRange(`${columnLetter}${i + 1}`).setNumberFormat('@');
         }
+        newSheet.getRange(`${columnLetter}${i + 1}`).setValue(lst[i][j]);
+      }
     }
-}
+  }
